f49db9617c3f7c3aaf64f9bded64ac35b84f2d12
Robert Andreev <andreev.robert.a@gmail.com>
2020-10-31 23:08:37 +0300

    Add Matrix parsing from file

--- src/main/java/ru/spbstu/telematics/java/App.java
+++ src/main/java/ru/spbstu/telematics/java/App.java
@@ -1,13 +1,28 @@
 package ru.spbstu.telematics.java;
 
+import java.io.FileReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        if(args.length == 2) {
+
+            try {
+                Matrix matrix1 = new Matrix(args[0]);
+                Matrix matrix2 = new Matrix(args[1]);
+                matrix1.multiplication(matrix2);
+            }
+            catch (Exception ex) {
+
+            }
+
+        } else {
+            System.out.println("Require 2 arguments. There are " + args.length);
+        }
+
     }
 }
---
+++ src/main/java/ru/spbstu/telematics/java/Matrix.java
@@ -0,0 +1,58 @@
+package ru.spbstu.telematics.java;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matrix {
+    private double [][] matrix;
+
+    Matrix (int stringsSize, int columnsSize) {
+        matrix = new double[stringsSize][columnsSize];
+    }
+
+    public Matrix (String filePath) {
+        int stringsSize = 0;
+        int columnsSize = 0;
+
+        try (Scanner Walk = new Scanner(new FileReader(filePath))) {
+            while (Walk.hasNextLine() && Walk.hasNextDouble()){
+
+                double[][] tmp = new double[++stringsSize][];
+                tmp = Arrays.copyOf(matrix,matrix.length);
+                matrix = tmp;
+                matrix[stringsSize - 1] = stringToArray(Walk.nextLine());
+                if (columnsSize == 0) {
+                    columnsSize = matrix[stringsSize - 1].length;
+                }
+                else {
+                    if (columnsSize != matrix[stringsSize - 1].length) {
+                        throw new RuntimeException();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private double[] stringToArray(String str) {
+        String[] mas = str.split(" ");
+        double[] res = new double[mas.length];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = Double.parseDouble(mas[i]);
+        }
+        return res;
+    }
+
+    public Matrix multiplication(Matrix other) {
+        if (this.matrix[0].length != other.matrix.length) {
+            throw new RuntimeException();
+        }
+        Matrix res = new Matrix(this.matrix.length, other.matrix[0].length);
+        //to do: multiply
+        return res;
+    }
+
+}
