60a2765455e529edb5859f8cd87cab0fd41f685b
Robert Andreev <andreev.robert.a@gmail.com>
2020-11-09 16:03:41 +0300

    Add files and dependencies

---
+++ src/main/java/ru/spbstu/telematics/java/MyTreeMap.java
@@ -0,0 +1,8 @@
+package ru.spbstu.telematics.java;
+
+public class MyTreeMap {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
---
+++ src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
@@ -0,0 +1,15 @@
+package ru.spbstu.telematics.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MyFirstJUnitJupiterTests {
+
+    @Test
+    void Test1() {
+        System.out.println(2);
+    }
+
+
+}
