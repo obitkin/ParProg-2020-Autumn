656b069401356648685b23c04784a919bd4b4f23
Robert Andreev <andreev.robert.a@gmail.com>
2020-11-13 16:48:16 +0300

    Base for Tests

--- src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
+++ src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
@@ -2,13 +2,34 @@ package ru.spbstu.telematics.java;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.util.TreeMap;
+import java.util.Random;
 
-class MyFirstJUnitJupiterTests {
+class MyTreeMapTest {
+
+    static int size = 50;
+    static int max = 100;
+    static final Random random = new Random();
+
+    static TreeMap<String,Double> treeMap;
+    static MyTreeMap<String,Double> myTreeMap;
+
+
+    @BeforeAll
+    static void setUp() {
+        treeMap = new TreeMap<>();
+        myTreeMap = new MyTreeMap<>();
+        for (int i = 0; i < size; i++ ) {
+            treeMap.put(String.valueOf(random.nextInt(max)), (double) i);
+            myTreeMap.put(String.valueOf(random.nextInt(max)), (double) i);
+        }
+    }
 
     @Test
-    void Test1() {
-        System.out.println(2);
+    void Test() {
+        //assertEquals(treeMap.size(),myTreeMap.size());
     }
 
 
