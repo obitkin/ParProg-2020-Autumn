5e11aac8bbb5bfde5776a3ee7fe1a0352bfbf3f5
Robert Andreev <andreev.robert.a@gmail.com>
2020-11-13 16:51:50 +0300

    Add base for MyTreeMap

--- src/main/java/ru/spbstu/telematics/java/MyTreeMap.java
+++ src/main/java/ru/spbstu/telematics/java/MyTreeMap.java
@@ -1,8 +1,164 @@
 package ru.spbstu.telematics.java;
 
-public class MyTreeMap {
+import java.util.*;
+import java.util.TreeMap;
 
-    public static void main(String[] args) {
-        System.out.println(1);
+public class MyTreeMap<K,V>
+        extends AbstractMap<K,V>
+        implements NavigableMap<K,V>,
+        Iterable<K> {
+
+    static final class Entry<K,V> implements Map.Entry<K,V> {
+        K key;
+        V value;
+        Entry<K, V> left;
+        Entry<K, V> right;
+        Entry<K, V> parent;
+        boolean color = true;
+
+        @Override
+        public K getKey() {
+            return null;
+        }
+
+        @Override
+        public V getValue() {
+            return null;
+        }
+
+        @Override
+        public V setValue(V value) {
+            return null;
+        }
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> lowerEntry(K key) {
+        return null;
+    }
+
+    @Override
+    public K lowerKey(K key) {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> floorEntry(K key) {
+        return null;
+    }
+
+    @Override
+    public K floorKey(K key) {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> ceilingEntry(K key) {
+        return null;
+    }
+
+    @Override
+    public K ceilingKey(K key) {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> higherEntry(K key) {
+        return null;
+    }
+
+    @Override
+    public K higherKey(K key) {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> firstEntry() {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> lastEntry() {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> pollFirstEntry() {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> pollLastEntry() {
+        return null;
+    }
+
+    @Override
+    public NavigableMap<K, V> descendingMap() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<K> navigableKeySet() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<K> descendingKeySet() {
+        return null;
+    }
+
+    @Override
+    public NavigableMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey, boolean toInclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableMap<K, V> headMap(K toKey, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableMap<K, V> tailMap(K fromKey, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public Comparator<? super K> comparator() {
+        return null;
+    }
+
+    @Override
+    public SortedMap<K, V> subMap(K fromKey, K toKey) {
+        return null;
+    }
+
+    @Override
+    public SortedMap<K, V> headMap(K toKey) {
+        return null;
+    }
+
+    @Override
+    public SortedMap<K, V> tailMap(K fromKey) {
+        return null;
+    }
+
+    @Override
+    public K firstKey() {
+        return null;
+    }
+
+    @Override
+    public K lastKey() {
+        return null;
     }
 }
