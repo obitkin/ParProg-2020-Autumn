ceee8233bc446ad4e1a9620694e05691188dd92f
Robert Andreev <andreev.robert.a@gmail.com>
2020-11-14 15:38:57 +0300

    Add small tests for Map(0.5/3)

--- src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
+++ src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
@@ -4,33 +4,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import java.util.TreeMap;
-import java.util.Random;
+
+import java.util.*;
 
 class MyTreeMapTest {
 
-    static int size = 50;
+    static int size = 3;
     static int max = 100;
     static final Random random = new Random();
 
-    static TreeMap<String,Double> treeMap;
-    static MyTreeMap<String,Double> myTreeMap;
-
+    static Map<String,Double> treeMap;
+    static Map<String,Double> myTreeMap;
 
     @BeforeAll
     static void setUp() {
         treeMap = new TreeMap<>();
         myTreeMap = new MyTreeMap<>();
         for (int i = 0; i < size; i++ ) {
-            treeMap.put(String.valueOf(random.nextInt(max)), (double) i);
-            myTreeMap.put(String.valueOf(random.nextInt(max)), (double) i);
+            myTreeMap.put(String.valueOf(i*i), (double) i);
+            treeMap.put(String.valueOf(i*i), (double) i);
         }
     }
 
     @Test
-    void Test() {
+    void TestForMap() {
+        assertEquals(treeMap.size(),myTreeMap.size());
+        Set<Map.Entry<String,Double>> t1 = treeMap.entrySet();
+        for (Map.Entry<String, Double> i : t1) {
+            i.setValue((double)(i.getValue()+6));
+        }
+        Set<Map.Entry<String,Double>> t2 = myTreeMap.entrySet();
+        for (Map.Entry<String, Double> i : t2) {
+            i.setValue((double)i.getValue()+3);
+        }
+    }
+
+    @Test
+    void TestForSortedMap() {
         //assertEquals(treeMap.size(),myTreeMap.size());
     }
 
+    @Test
+    void TestForNavigableMap() {
+        //assertEquals(treeMap.size(),myTreeMap.size());
+    }
 
 }
