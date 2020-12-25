3df4964afb48010ab0da99d21297a5602e018872
Robert Andreev <andreev.robert.a@gmail.com>
2020-12-25 20:10:50 +0300

    Add random modification(put/remove) Map test

--- src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
+++ src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
@@ -4,67 +4,130 @@ import org.junit.*;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
 import static org.junit.Assert.*;
 
 import java.util.*;
 
 public class MyTreeMapTest {
 
-    static int size = 10;
-    static int Mod = 0;
-    static final Random random = new Random();
+    static int size = 25;
+    static int Mod = 150;
+    static final Random random = new Random(2);
+
+    String value = new String("Const Value for put without adding new entry");
 
     static Map<Double,String> treeMap;
     static Map<Double,String> myTreeMap;
 
-
     static void setUp() {
         treeMap = new TreeMap<>();
         myTreeMap = new MyTreeMap<>();
+
         for (int i = 0; i < size; i++ ) {
             myTreeMap.put((double) i, String.valueOf(i*i));
-            treeMap.put((double) i,String.valueOf(i*i));
+            treeMap.put((double) i, String.valueOf(i*i));
         }
     }
 
     void CheckEquals(Map<?,?> left, Map<?,?> right) {
+        if (left == null && right == null)
+            return;
 
         assertEquals(left.isEmpty(),right.isEmpty());
         assertEquals(left.size(),right.size());
 
-        Iterator<?> it1 = left.keySet().iterator();
-        Iterator<?> it2 = right.keySet().iterator();
-        System.out.println("\n");
-        while (it1.hasNext() && it2.hasNext()) {
-            Object o1 = it1.next();
-            Object o2 = it2.next();
+        Iterator<?> KeyIterator1 = left.keySet().iterator();
+        Iterator<?> KeyIterator2 = right.keySet().iterator();
 
-            assertEquals(o1,o2);
-            assertEquals(left.get(o1),left.get(o2));
-            System.out.println(o1.toString() + ": " + left.get(o1) + "   |   " + o2.toString() + " : " + left.get(o2));
+        while (KeyIterator1.hasNext() || KeyIterator2.hasNext()) {
+            Object o1;
+            Object o2;
+            if (KeyIterator1.hasNext() && KeyIterator2.hasNext()) {
+                o1 = KeyIterator1.next();
+                o2 = KeyIterator2.next();
+                System.out.println(o1.toString() + ": " + left.get(o1) + "   |   " + o2.toString() + " : " + right.get(o2));
+            }
+            else if (KeyIterator1.hasNext()) {
+                o1 = KeyIterator1.next();
+                System.out.println(o1.toString() + ": " + left.get(o1));
+            }
+            else {
+                o2 = KeyIterator2.next();
+                System.out.println(o2.toString() + ": " + right.get(o2));
+            }
         }
 
-        if (it1.hasNext() != it2.hasNext())
+        KeyIterator1 = left.keySet().iterator();
+        KeyIterator2 = right.keySet().iterator();
+
+        while (KeyIterator1.hasNext() && KeyIterator2.hasNext()) {
+            Object o1 = KeyIterator1.next();
+            Object o2 = KeyIterator2.next();
+            assertEquals(o1,o2);
+            assertEquals(left.get(o1),right.get(o2));
+        }
+        if (KeyIterator1.hasNext() != KeyIterator2.hasNext())
+            fail();
+
+        Iterator<?> ValueIterator1 = left.values().iterator();
+        Iterator<?> ValueIterator2 = right.values().iterator();
+
+        while (ValueIterator1.hasNext() && ValueIterator2.hasNext()) {
+            Object o1 = ValueIterator1.next();
+            Object o2 = ValueIterator2.next();
+            assertEquals(o1,o2);
+            assertEquals(left.containsValue(o1),right.containsValue(o2));
+        }
+        if (ValueIterator1.hasNext() != ValueIterator2.hasNext())
+            fail();
+
+        Iterator<?> EntryIterator1 = left.entrySet().iterator();
+        Iterator<?> EntryIterator2 = right.entrySet().iterator();
+
+        while (EntryIterator1.hasNext() && EntryIterator2.hasNext()) {
+            Object o1 = EntryIterator1.next();
+            Object o2 = EntryIterator2.next();
+            assertEquals(o1,o2);
+        }
+        if (EntryIterator1.hasNext() != EntryIterator2.hasNext())
             fail();
     }
 
     @Test
     public void TestForMap() {
-
         setUp();
-
         CheckEquals(treeMap,myTreeMap);
 
-        treeMap.remove(new Double(1));
-        myTreeMap.remove(new Double(1));
+        System.out.println("\n\n\n");
 
-        for (int i = 0; i < size /2; i++){
-            treeMap.remove(new Double(i));
-            myTreeMap.remove(new Double(i));
+        /* 10000 random operation on TreeMap and MyTreemap
+        *  50% for put
+        *  50% for remove
+        * */
+
+        for (int i = 0; i < 10000; i++){
+            int r  = random.nextInt(250);
+            boolean add = random.nextBoolean();
+            boolean constValue = random.nextBoolean();
+            if (add) {
+                if (constValue) {
+                    assertEquals(treeMap.put((double)r,value),
+                            myTreeMap.put((double)r,value));
+                }
+                else {
+                    //System.out.println("Add K: " + r + "   V: " + String.valueOf(r*r*r)); debug
+                    assertEquals(treeMap.put((double)r,String.valueOf(r*r*r)),
+                            myTreeMap.put((double)r,String.valueOf(r*r*r)));
+                }
+            }
+            else {
+                //System.out.println("-Del K: " + r); debug
+                assertEquals(treeMap.remove((double)r),
+                myTreeMap.remove((double)r));
+            }
         }
-
         CheckEquals(treeMap,myTreeMap);
-
 /*
         Set<Double> k1 = treeMap.keySet();
         Set<Double> k2 = myTreeMap.keySet();
@@ -131,6 +194,8 @@ public class MyTreeMapTest {
 
 */
     }
+
+
 
 
 }
