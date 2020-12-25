3dcce2bdf1eaff3108727828f4c7f0f25adba4eb
Robert Andreev <andreev.robert.a@gmail.com>
2020-12-25 18:32:35 +0300

    Delete old useless tests

--- src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
+++ src/test/java/ru/spbstu/telematics/java/MyTreeMapTest.java
@@ -1,37 +1,73 @@
 package ru.spbstu.telematics.java;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.*;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
 
 import java.util.*;
 
-class MyTreeMapTest {
+public class MyTreeMapTest {
 
     static int size = 10;
     static int Mod = 0;
     static final Random random = new Random();
 
-    static Map<String,Double> treeMap;
-    static MyTreeMap<String,Double> myTreeMap;
+    static Map<Double,String> treeMap;
+    static Map<Double,String> myTreeMap;
 
-    @BeforeAll
+
     static void setUp() {
         treeMap = new TreeMap<>();
         myTreeMap = new MyTreeMap<>();
         for (int i = 0; i < size; i++ ) {
-            myTreeMap.put(String.valueOf(i*i), (double) i);
-            treeMap.put(String.valueOf(i*i), (double) i);
+            myTreeMap.put((double) i, String.valueOf(i*i));
+            treeMap.put((double) i,String.valueOf(i*i));
         }
     }
 
+    void CheckEquals(Map<?,?> left, Map<?,?> right) {
+
+        assertEquals(left.isEmpty(),right.isEmpty());
+        assertEquals(left.size(),right.size());
+
+        Iterator<?> it1 = left.keySet().iterator();
+        Iterator<?> it2 = right.keySet().iterator();
+        System.out.println("\n");
+        while (it1.hasNext() && it2.hasNext()) {
+            Object o1 = it1.next();
+            Object o2 = it2.next();
+
+            assertEquals(o1,o2);
+            assertEquals(left.get(o1),left.get(o2));
+            System.out.println(o1.toString() + ": " + left.get(o1) + "   |   " + o2.toString() + " : " + left.get(o2));
+        }
+
+        if (it1.hasNext() != it2.hasNext())
+            fail();
+    }
+
     @Test
-    void TestForMap() {
-        assertEquals(treeMap.isEmpty(),myTreeMap.isEmpty());
-        assertEquals(treeMap.size(),myTreeMap.size());
-        Set<String> k1 = treeMap.keySet();
-        Set<String> k2 = myTreeMap.keySet();
+    public void TestForMap() {
+
+        setUp();
+
+        CheckEquals(treeMap,myTreeMap);
+
+        treeMap.remove(new Double(1));
+        myTreeMap.remove(new Double(1));
+
+        for (int i = 0; i < size /2; i++){
+            treeMap.remove(new Double(i));
+            myTreeMap.remove(new Double(i));
+        }
+
+        CheckEquals(treeMap,myTreeMap);
+
+/*
+        Set<Double> k1 = treeMap.keySet();
+        Set<Double> k2 = myTreeMap.keySet();
 
         for (String i : k1) {
             System.out.println(i + " " + treeMap.get(i));
@@ -93,7 +129,7 @@ class MyTreeMapTest {
         treeMap.clear();
         myTreeMap.clear();
 
-
+*/
     }
 
 
