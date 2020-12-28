ea952b1c48c22fb34900dc51d384f22a7e583739
Robert Andreev <andreev.robert.a@gmail.com>
2020-12-28 03:46:17 +0300

    Add return extra logic

--- src/main/java/ru/spbstu/telematics/java/Neighbor.java
+++ src/main/java/ru/spbstu/telematics/java/Neighbor.java
@@ -3,14 +3,22 @@ import java.util.Random;
 
 public class Neighbor implements Runnable {
 
+    private Boolean flag;
     private Neighbor neighbor = null;
-    private int berries;
-    private final Random random = new Random();
+    private int berries = 0;
+    private int maxBerries;
     private Field field;
+    private int timeOut;
+    private final Random random = new Random();
 
-    public Neighbor(int berries, Field field) {
-        this.berries = berries;
+    public Neighbor(Field field, int timeOut, Boolean flag, int maxBerries) {
+        if (timeOut <= 0 || maxBerries <= 0)
+            throw new IllegalArgumentException();
+
         this.field = field;
+        this.timeOut = timeOut;
+        this.flag = flag;
+        this.maxBerries = maxBerries;
     }
 
     void setNeighbor(Neighbor neighbor) {
@@ -29,25 +37,40 @@ public class Neighbor implements Runnable {
 
             System.out.println(Thread.currentThread().getName() + " Before: " + berries);
 
-            synchronized (field) {
-                while (berries > neighbor.progress() && !field.isEmpty()) {
+            synchronized (flag) {
+                while (berries > neighbor.progress()) {
                     try {
                         System.out.println(Thread.currentThread().getName() + " Is waiting: " + berries);
-                        field.wait(random.nextInt(1000));
+                        flag.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                field.notify();
-                berries += field.getSomeBerries();
+                synchronized (field) {
+                    berries += field.getSomeBerries();
+                    if (berries >= maxBerries) {
+                        int extraBerries = berries - maxBerries;
+                        if (extraBerries > 0) {
+                            field.returnSomeBerries(extraBerries);
+                            berries -= extraBerries;
+                        }
+                    }
+                }
+                flag.notify();
             }
 
-            if (field.isEmpty()) {
+            System.out.println(Thread.currentThread().getName() + " After:  " + berries);
+
+            if (berries == maxBerries) {
                 System.out.println(Thread.currentThread().getName() + " is terminated: " + berries);
                 break;
             }
 
-            System.out.println(Thread.currentThread().getName() + " After:  " + berries);
+            try {
+                Thread.sleep(timeOut);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
