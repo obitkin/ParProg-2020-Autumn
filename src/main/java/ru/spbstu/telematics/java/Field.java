3fe59860225333cd698d4f2b2dc44c4c95ff56e1
Robert Andreev <andreev.robert.a@gmail.com>
2020-12-28 03:45:11 +0300

    Add put berries methods and fix get

--- src/main/java/ru/spbstu/telematics/java/Field.java
+++ src/main/java/ru/spbstu/telematics/java/Field.java
@@ -5,25 +5,60 @@ import java.util.Random;
 public class Field {
     int berries;
     int bound;
+    int maxBerries;
+    int producedBerries = 0;
     final Random random = new Random();
 
-    public Field(int berries, int bound) {
+    public Field(int bound, int berries, int maxBerries) {
+        if (bound <= 0 || berries <= 0 || maxBerries <= 0)
+            throw new IllegalArgumentException();
+
         this.berries = berries;
-        this.bound = (bound <= 0)? 5: bound;
+        this.bound = bound;
+        this.maxBerries = maxBerries;
+    }
+
+    synchronized void returnSomeBerries(int growing) {
+        while (berries >= maxBerries) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.berries += Math.min(growing, maxBerries-berries);
+        notifyAll();
+    }
+
+    synchronized void addSomeBerries(int growing) {
+        while (berries >= maxBerries) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        producedBerries += Math.min(growing, maxBerries-berries);
+        this.berries += Math.min(growing, maxBerries-berries);
+        notifyAll();
     }
 
     int getSomeBerries() {
-        if (berries > 0) {
-            System.out.println("Field before: " + berries);
-            int r = Math.min(berries, random.nextInt(5));
-            berries -= r;
-            System.out.println("Field after:  " + berries);
-            return r;
-        }
-        else {
+        while (berries < 1) {
             System.out.println("Field is empry: " + berries);
-            return 0;
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println("Field before: " + berries);
+        int r = Math.min(berries, random.nextInt(bound));
+        berries -= r;
+        System.out.println("Field after:  " + berries);
+        notifyAll();
+        return r;
+
     }
 
     boolean isEmpty() {
