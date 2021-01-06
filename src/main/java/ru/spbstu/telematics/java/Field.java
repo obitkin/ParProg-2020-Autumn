1684a0e35539e64994fa5ccd43835177bfa84457
Robert Andreev <andreev.robert.a@gmail.com>
2021-01-06 19:37:06 +0500

    Remove return and fix get

--- src/main/java/ru/spbstu/telematics/java/Field.java
+++ src/main/java/ru/spbstu/telematics/java/Field.java
@@ -10,24 +10,12 @@ public class Field {
     final Random random = new Random();
 
     public Field(int bound, int berries, int maxBerries) {
-        if (bound <= 0 || berries <= 0 || maxBerries <= 0)
+        if (bound <= 1 || berries <= 0 || maxBerries <= 0)
             throw new IllegalArgumentException();
 
         this.berries = berries;
         this.bound = bound;
         this.maxBerries = maxBerries;
-    }
-
-    synchronized void returnSomeBerries(int growing) {
-        while (berries >= maxBerries) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        this.berries += Math.min(growing, maxBerries-berries);
-        notifyAll();
     }
 
     synchronized void addSomeBerries(int growing) {
@@ -43,7 +31,7 @@ public class Field {
         notifyAll();
     }
 
-    int getSomeBerries() {
+    synchronized int getSomeBerries(int maxBerriesCanTake) {
         while (berries < 1) {
             System.out.println("Field is empry: " + berries);
             try {
@@ -53,12 +41,12 @@ public class Field {
             }
         }
         System.out.println("Field before: " + berries);
-        int r = Math.min(berries, random.nextInt(bound));
-        berries -= r;
+        int taken = Math.min(berries, random.nextInt(bound));
+        taken = Math.min(taken, maxBerriesCanTake);
+        berries -= taken;
         System.out.println("Field after:  " + berries);
         notifyAll();
-        return r;
-
+        return taken;
     }
 
     boolean isEmpty() {
