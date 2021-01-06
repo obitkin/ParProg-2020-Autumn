b9d4a70c65d1f873550ae1babb60c5695ce85c14
Robert Andreev <andreev.robert.a@gmail.com>
2021-01-06 19:39:16 +0500

    Small fix

--- src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -9,24 +9,24 @@ public class AppTest {
     //Parameters for Field
     static int numberOfBerries = 10000; //Изначальное число ягод на поле
     static int numberOfMaxBerries= 20000; //Максимальное число ягод на поле
-    static int boundTakenBerriesInOneTime = 50; //Число ягод, которое можно забрать за раз (rand(boundTakenBerriesInOneTime))
+    static int boundTakenBerriesInOneTime = 10; //Число ягод >1, которое можно забрать за раз (rand(boundTakenBerriesInOneTime))
 
     //Parameters for Bushes
-    static int timeOutBush = 10; //Период добавления новых ягод
-    static int boundProducedBerriesInOneTime = 100; //Число ягод, которое которые выросли за раз (rand(boundProducedBerriesInOneTime))
+    static int timeOutBush = 0; //Период добавления новых ягод
+    static int boundProducedBerriesInOneTime = 100; //Число ягод >1, которое которые выросли за раз (rand(boundProducedBerriesInOneTime))
 
     //Parameters for Neighbors
     static int timeOutNeighbor = 10; //Период сбора ягод
-    static int maxNeighborBerries = 3000; //Максимальное количество ягод у Neighbor, при достижении заканчивает
+    static int maxNeighborBerries = 6000; //Максимальное количество ягод у Neighbor, при достижении заканчивает
 
     @Test
     public void ConcurrencyTest() {
         Field field = new Field(boundTakenBerriesInOneTime,numberOfBerries,numberOfMaxBerries);
 
-        Boolean flag = new Boolean(true);
+        Flag flag = new Flag();
 
-        Neighbor N1 = new Neighbor(field,timeOutNeighbor,flag,maxNeighborBerries);
-        Neighbor N2 = new Neighbor(field,timeOutNeighbor,flag,maxNeighborBerries);
+        Neighbor N1 = new Neighbor(field,flag,timeOutNeighbor,maxNeighborBerries);
+        Neighbor N2 = new Neighbor(field,flag,timeOutNeighbor,maxNeighborBerries);
         Bush B1 = new Bush(field,boundProducedBerriesInOneTime,timeOutBush);
 
         N1.setNeighbor(N2);
