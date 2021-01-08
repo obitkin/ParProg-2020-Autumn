52f8ce970ffb12def24f8fe96f6c9d2912cbf0f4
Robert Andreev <andreev.robert.a@gmail.com>
2021-01-08 14:53:10 +0500

    Small fix for test

--- src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -14,36 +14,37 @@ public class AppTest {
     double[][] A;
     double[][] B;
 
-    int row = 100;
-    int column = 100;
+    int row = 1024;
+    int column = 1024;
 
     @Test
     public void ConcurrencyTest() {
+        System.out.println("Row = column = " + row);
         A = new double[row][column];
         B = new double[row][column];
 
-        //int g = Runtime.getRuntime().availableProcessors();
-        //System.out.println(g);
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 A[i][j] = random.nextDouble();
                 B[i][j] = random.nextDouble();
             }
         }
+
         long t = System.currentTimeMillis();
-        double[][] res1 = Multiplier.multiplyParallel(A,B);
-        System.out.println("Parallel execution time " + (System.currentTimeMillis() - t));
-
-
-
-        t = System.currentTimeMillis();
         double[][] res2 = Multiplier.multiplySerial(A,B);
         System.out.println("Serial execution time   " + (System.currentTimeMillis() - t));
 
+        t = System.currentTimeMillis();
+        double[][] res1 = Multiplier.multiplyParallel(A,B);
+        System.out.println("Parallel execution time " + (System.currentTimeMillis() - t));
+
         //System.out.println(Arrays.deepToString(res1));
         //System.out.println(Arrays.deepToString(res2));
+        t = System.currentTimeMillis();
         for (int i = 0; i < row; i++)
             assertArrayEquals(res1[i],res2[i],0.001);
+        System.out.println("Equals time " + (System.currentTimeMillis() - t));
+        System.out.println("------------------------");
     }
 
 }
