98af6153a22fabffe7796c277c6c2925f1b74597
Robert Andreev <andreev.robert.a@gmail.com>
2021-01-08 19:26:19 +0500

    Refactor tests and add rectangular matrices

--- src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -31,29 +31,81 @@ public class AppTest {
         }
     }
 
-    @Test
-    public void ConcurrencyTest() {
-        System.out.println("Square matrix:");
-        int m = 1, n = 1, l = 1;
+    void test(int m, int n, int l) {
+        init(m, n, l);
+        System.out.println("  " + m + "*" + n + " x " + n + "*" + l);
+        init(m, n, l);
 
-        for (int i = 1; i <= 12; i++) {
-            m = n = l = l * 2;
-            System.out.println("i = " + i + "  " + m + "*" + n + " x " + n + "*" + l);
-            init(m, n, l);
+        long t = System.currentTimeMillis();
+        double[][] resSerial = Multiplier.multiplySerial(A,B);
+        System.out.println("Serial execution time   " + (System.currentTimeMillis() - t));
 
-            long t = System.currentTimeMillis();
-            double[][] res2 = Multiplier.multiplySerial(A,B);
-            System.out.println("Serial execution time   " + (System.currentTimeMillis() - t));
-
+        double[][] resParallel = null;
+        for (int k = 2; k <= 8; k *= 2) {
             t = System.currentTimeMillis();
-            double[][] res1 = Multiplier.multiplyParallel(A,B);
-            System.out.println("Parallel execution time " + (System.currentTimeMillis() - t));
+            resParallel = Multiplier.multiplyParallel(A,B,k);
+            System.out.println("Threads " + k + "  " + (System.currentTimeMillis() - t));
 
-            for (int j = 0; j < m; j++)
-                assertArrayEquals(res1[j],res2[j],0.00001);
+            check(resSerial,resParallel);
+        }
+        System.out.println("------------------------------");
+    }
 
-            System.out.println("------------------------");
+    void check(double[][] resSerial, double[][] resParallel) {
+
+        //first check
+        for (int j = 0; j < resSerial.length; j++)
+            assertArrayEquals(resSerial[j],resParallel[j],0.00001);
+
+        //second check
+        if (resSerial.length != resParallel.length) {
+            fail();
+        }
+        else {
+            for (int i = 0; i < resSerial.length; i++) {
+                if (resSerial[i].length != resParallel[i].length) {
+                    fail();
+                }
+                else {
+                    for (int j = 0; j < resSerial[i].length; j++) {
+                        if (Double.compare(resSerial[i][j],resParallel[i][j]) != 0)
+                            fail();
+                    }
+                }
+            }
         }
     }
 
+    @Test
+    public void MatrixMulTest() {
+        System.out.println("Square matrices:");
+        int m = 1, n = 1, l = 1;
+
+        for (int i = 1; i <= 12; i++) { //from 2^0 to 2^(i-1)
+            test(m, n, l);
+            m = n = l = l * 2;
+        }
+
+        System.out.println("Rectangular matrices:");
+        m = 1; n = 10000; l = 1;
+        test(m, n, l);
+        m = 250; n = 100; l = 150;
+        test(m, n, l);
+        m = 500; n = 200; l = 300;
+        test(m, n, l);
+        m = 750; n = 300; l = 500;
+        test(m, n, l);
+        m = 800; n = 500; l = 1000;
+        test(m, n, l);
+        m = 900; n = 700; l = 1000;
+        test(m, n, l);
+        m = 1500; n = 1000; l = 900;
+        test(m, n, l);
+        m = 2000; n = 1500; l = 1500;
+        test(m, n, l);
+        m = 2000; n = 2000; l = 2500;
+        test(m, n, l);
+
+
+    }
 }
